6e06e916eb780cd23cf932242373c9db14aacd6b
Олеся Утарова <Ulandy.RB@hotmail.com>
2021-09-26 22:43:08 +0300

    Add OrderListener and Controller

---
+++ core/src/main/java/ru/digitalleague/core/contoller/OrderController.java
@@ -0,0 +1,37 @@
+package ru.digitalleague.core.contoller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.digitalleague.core.model.OrderDetails;
+
+@RestController
+public class OrderController {
+
+    private RabbitTemplate rabbitTemplate;
+    public OrderController(@Autowired RabbitTemplate rabbitTemplate)
+    {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping("/orders")
+    public void addOrder(@RequestBody OrderDetails orderDetails)
+    {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            String order = objectMapper.writeValueAsString(orderDetails);
+            rabbitTemplate.convertAndSend("order", order);
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
