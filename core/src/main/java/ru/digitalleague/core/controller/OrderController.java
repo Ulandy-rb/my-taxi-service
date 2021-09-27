4a5dcf7f3b2d084a7112d841b59fa30ea7a13c58
Олеся Утарова <Ulandy.RB@hotmail.com>
2021-09-27 19:42:17 +0300

    Added OrderListener and Controller

--- core/src/main/java/ru/digitalleague/core/controller/OrderController.java
+++ core/src/main/java/ru/digitalleague/core/controller/OrderController.java
@@ -1,24 +1,18 @@
-package ru.digitalleague.core.contoller;
+package ru.digitalleague.core.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.digitalleague.core.model.OrderDetails;
 
 @RestController
 public class OrderController {
 
-    private RabbitTemplate rabbitTemplate;
-    public OrderController(@Autowired RabbitTemplate rabbitTemplate)
-    {
-        this.rabbitTemplate = rabbitTemplate;
-    }
+    @Autowired private RabbitTemplate rabbitTemplate;
 
     @PostMapping("/order-taxi")
     public void addOrder(@RequestBody OrderDetails orderDetails)
