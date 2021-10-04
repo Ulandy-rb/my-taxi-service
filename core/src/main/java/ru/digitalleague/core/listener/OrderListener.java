11d196e3e04bfc5cbafb30b6b582b3ebcd4c8ac4
Олеся Утарова <Ulandy.RB@hotmail.com>
2021-10-04 10:22:19 +0300

    Config changes

--- core/src/main/java/ru/digitalleague/core/listener/OrderListener.java
+++ core/src/main/java/ru/digitalleague/core/listener/OrderListener.java
@@ -1,7 +1,11 @@
 package ru.digitalleague.core.listener;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
+@Component
+@EnableRabbit
 public class OrderListener {
     @RabbitListener(queues = "order")
     public void listen(String in) {
